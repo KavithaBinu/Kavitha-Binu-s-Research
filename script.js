efda5d3dee186013565192dfaa919e726fc4c1e5
KavithaBinu <iamkavitha.kk@gmail.com>
2026-09-21 20:46:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// This listens for a click on our button and triggers a pop-up alert
+document.getElementById("helloBtn").addEventListener("click", function() {
+    alert("Hello! Thanks for visiting my new website.");
+});
